c5a6eb4fe98b1a62191c78eb01e8c88a8bb0beaf
HamzaAryanSapnil <139946462+HamzaAryanSapnil@users.noreply.github.com>
2024-05-12 21:14:26 +0600

    top food header done

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../../Components/Banner/Banner";
+import TopFoodsSection from "./Top Foods Section/TopFoodsSection";
 
 
 const Home = () => {
     return (
-        <div>
+        <div className="space-y-14" >
            <Banner/>
+           <TopFoodsSection/>
         </div>
     );
 };
---
+++ src/Pages/Home/Top Foods Section/TopFoodsSection.jsx
@@ -0,0 +1,14 @@
+
+
+const TopFoodsSection = () => {
+    return (
+        <div className="my-14 w-full p-4" >
+            <div className="md:w-1/2 mx-auto gap-y-4 flex flex-col justify-center items-center" >
+                <h1 className="md:text-5xl text-3xl font-black" >Top Foods</h1>
+                <p className="text-lg text-justify" >Indulge in a symphony of flavors with our top food items showcase! From savory delights to mouthwatering treats, explore a culinary journey curated just for you. Savor the essence of each dish as we bring you the finest selection of gastronomic delights to tantalize your taste buds and elevate your dining experience.</p>
+            </div>
+        </div>
+    );
+};
+
+export default TopFoodsSection;
