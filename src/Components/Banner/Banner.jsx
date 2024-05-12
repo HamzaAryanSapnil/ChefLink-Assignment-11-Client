e98a77c6723997671a7a74671622a8ff17393125
HamzaAryanSapnil <139946462+HamzaAryanSapnil@users.noreply.github.com>
2024-05-12 20:46:14 +0600

    working with banner

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,11 +1,22 @@
+import bannerImg from "../../assets/hydrabadi biriyani.jpg"
+import bgImg from '../../assets/bg add food.jpg'
 const Banner = () => {
   return (
-    <div className="carousel w-full min-h-screen">
-      <div id="slide1" className="carousel-item relative w-full">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg"
-          className="w-full"
-        />
+    <div className=" carousel w-full min-h-screen">
+      <div
+      style={{backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)),url(${bgImg})`}}
+      id="slide1" className="carousel-item relative w-full bg-cover bg-center bg-[#fbf9f0]">
+        
+         <div className=" bg-opacity-60"></div>
+        <div className="absolute flex justify-around transform -translate-y-1/2 left-5 right-5 top-1/2">
+        <div className="max-w-sm flex flex-col justify-center items-start gap-4 p-2 text-black">
+            <h1 className="text-3xl md:text-5xl font-bold" ><span className="bg-gradient-to-bl from-[#ffdbb5] to-[#873a06] bg-clip-text text-transparent" >ChefLink</span>. Your Very Own Recipe Network</h1>
+            <p className="text-lg" >Delicious dishes, unforgettable moments.</p>
+            </div>
+            <div>
+                <img src={bannerImg} className="max-w-md rounded-xl" alt="" />
+            </div>
+        </div>
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a href="#slide4" className="btn btn-circle">
             ❮
