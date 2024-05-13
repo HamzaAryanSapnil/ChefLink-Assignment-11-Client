9d4617d60c3b266bb2bcdc119501578e35785c72
HamzaAryanSapnil <139946462+HamzaAryanSapnil@users.noreply.github.com>
2024-05-13 14:34:38 +0600

    error handled in my added food

--- src/Pages/My Added Foods/MyAddedFoods.jsx
+++ src/Pages/My Added Foods/MyAddedFoods.jsx
@@ -5,23 +5,42 @@ import MyAddedFoodsCard from "./MyAddedFoodsCard";
 const MyAddedFoods = () => {
   const { user } = useContext(AuthContext);
   const [data, setData] = useState([]);
-  const url = `http://localhost:5000/allFoodItems?email=${user?.email}`;
+  const [isLoading, setIsLoading] = useState(true);
+  const [url, setUrl] = useState("");
   useEffect(() => {
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setData(data);
-      });
-  }, []);
+    if (user?.email) {
+      setUrl(`http://localhost:5000/allFoodItems?email=${user.email}`);
+    }
+  }, [user]);
+
+  useEffect(() => {
+    if (url) {
+      fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          setData(data);
+          setIsLoading(false);
+        });
+    }
+  }, [url]);
+
   return (
-    <div className="container mx-auto" >
-      <h1>My Added Foods: {data.length}</h1>
-      <div className="flex flex-col justify-center items-center w-full" >
-        {data?.map((data) => (
-          <MyAddedFoodsCard key={data._id} data={data} ></MyAddedFoodsCard>
-        ))}
-      </div>
+    <div className="container mx-auto">
+      {isLoading ? (
+        <div className="flex justify-center items-center min-h-screen">
+          <span className="loading loading-spinner loading-lg"></span>
+        </div>
+      ) : (
+        <div>
+          <h1>My Added Foods: {data.length}</h1>
+          <div className="flex flex-col justify-center items-center w-full">
+            {data?.map((data) => (
+              <MyAddedFoodsCard key={data._id} data={data}></MyAddedFoodsCard>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- src/Pages/My Added Foods/MyAddedFoodsCard.jsx
+++ src/Pages/My Added Foods/MyAddedFoodsCard.jsx
@@ -1,22 +1,70 @@
-const MyAddedFoodsCard = () => {
+import PropTypes from 'prop-types'
+const MyAddedFoodsCard = ({data}) => {
+    console.log(data);
+    const {  foodName, foodImageUrl, foodCategory, price, quantity } = data;
   return (
-    <div className="card flex-row bg-base-100 shadow-xl">
-      <figure>
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-          alt="Shoes"
-          className="h-64 w-full"
-        />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
-        </div>
-      </div>
-    </div>
+    <div className="overflow-x-auto">
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+       
+        <th>Name</th>
+        <th>Job</th>
+        <th>Favorite Color</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      {/* row 1 */}
+      <tr>
+        <th>
+          <label>
+            <input type="checkbox" className="checkbox" />
+          </label>
+        </th>
+        <td>
+          <div className="flex items-center gap-3">
+            <div className="avatar">
+              <div className="mask mask-squircle w-12 h-12">
+                <img src="https://img.daisyui.com/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
+              </div>
+            </div>
+            <div>
+              <div className="font-bold">Hart Hagerty</div>
+              <div className="text-sm opacity-50">United States</div>
+            </div>
+          </div>
+        </td>
+        <td>
+          Zemlak, Daniel and Leannon
+          <br/>
+          <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
+        </td>
+        <td>Purple</td>
+        <th>
+          <button className="btn btn-ghost btn-xs">details</button>
+        </th>
+      </tr>
+     
+    </tbody>
+    {/* foot */}
+    <tfoot>
+      <tr>
+        <th></th>
+        <th>Name</th>
+        <th>Job</th>
+        <th>Favorite Color</th>
+        <th></th>
+      </tr>
+    </tfoot>
+    
+  </table>
+</div>
   );
 };
 
+MyAddedFoodsCard.propTypes = {
+  data: PropTypes.object,
+}
 export default MyAddedFoodsCard;
