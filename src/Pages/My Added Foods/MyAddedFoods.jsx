072d5b794265b568f8dd41cebde2083d8ca61735
HamzaAryanSapnil <139946462+HamzaAryanSapnil@users.noreply.github.com>
2024-05-13 14:18:54 +0600

    added my added cards

--- src/Pages/My Added Foods/MyAddedFoods.jsx
+++ src/Pages/My Added Foods/MyAddedFoods.jsx
@@ -1,22 +1,27 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../Auth Provider/AuthProvider";
+import MyAddedFoodsCard from "./MyAddedFoodsCard";
 
 const MyAddedFoods = () => {
-  const {user} = useContext(AuthContext);
-    const [data, setData] = useState([]);
-    const url = `http://localhost:5000/allFoodItems?email=${user?.email}`
-    useEffect(() => {
-        fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-            setData(data)
-        })
-    }, [])
+  const { user } = useContext(AuthContext);
+  const [data, setData] = useState([]);
+  const url = `http://localhost:5000/allFoodItems?email=${user?.email}`;
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setData(data);
+      });
+  }, []);
   return (
-    <div>
+    <div className="container mx-auto" >
       <h1>My Added Foods: {data.length}</h1>
-      
+      <div className="flex flex-col justify-center items-center w-full" >
+        {data?.map((data) => (
+          <MyAddedFoodsCard key={data._id} data={data} ></MyAddedFoodsCard>
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/Pages/My Added Foods/MyAddedFoodsCard.jsx
@@ -0,0 +1,22 @@
+const MyAddedFoodsCard = () => {
+  return (
+    <div className="card flex-row bg-base-100 shadow-xl">
+      <figure>
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+          alt="Shoes"
+          className="h-64 w-full"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">Shoes!</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MyAddedFoodsCard;
